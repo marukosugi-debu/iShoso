34fe667dd08404bab3fe0e561d89fc35f6b0ebab
Kosei Moriyama <cou929@gmail.com>
2010-05-30 16:05:39 +0900

    initial commit for iShoso-moz

---
+++ iShoso-moz/ishoso.js
@@ -0,0 +1,33 @@
+var orient = (function(ori) {
+  var x = 0;
+  var i = 0;
+
+  return function(ori) {
+    var div_x = x - ori.x;
+    
+    document.getElementById('ori-x').innerHTML = ori.x;
+    document.getElementById('ori-y').innerHTML = ori.y;
+    document.getElementById('ori-z').innerHTML = ori.z;
+
+    if (Math.abs(div_x) > 0.05) {
+      console.log(i++ + ' yes');
+    } else {
+    }
+    x = ori.x;
+
+    if (0.9 <= ori.x)
+      document.getElementById('shouso').innerHTML = '勝訴';
+    else if (-0.9 >= ori.x)
+      document.getElementById('shouso').innerHTML = '敗訴';
+    else
+      document.getElementById('shouso').innerHTML = '示談';
+
+    var dst = document.getElementById('dst');
+    dst.style.left = 100 + 100 * ori.x + "px";
+    dst.style.top = 100 + 100 * ori.y + "px";
+    console.log('rgb(' + (ori.z + 1) * 128 + ', 0, 0)');
+    dst.style.background = 'rgb(' + (ori.z + 1) * 128 + ', 0, 0)';
+  };
+})();
+
+window.addEventListener("MozOrientation", orient, true);
