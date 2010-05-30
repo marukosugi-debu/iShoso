51dbcc4d3be1d8c5b4fab5b54c1b43ed3bba67ef
Kosei Moriyama <cou929@gmail.com>
2010-05-30 20:59:43 +0900

    add description of this page

--- iShoso-moz/ishoso.js
+++ iShoso-moz/ishoso.js
@@ -1,19 +1,32 @@
 var orient = (function(ori) {
   var lose = false;
+  var size = {
+    shosoW: 387,
+    shosoH: 870,
+    haisoW: 378,
+    haisoH: 868
+  };
 
   return function(ori) {
     document.getElementById('ori-x').innerHTML = ori.x;
     document.getElementById('ori-y').innerHTML = ori.y;
     document.getElementById('ori-z').innerHTML = ori.z;
 
+    var judge = document.getElementById('judgement');
+    var judgeImg = document.getElementById('judgement-img');
+
     if (ori.z < 0 && !lose) {
-      document.getElementById('judgement-img').setAttribute('src', 'haiso.png');
-      document.getElementById('judgement-img').setAttribute('alt', '敗訴');
+      judgeImg.setAttribute('src', 'haiso.png');
+      judgeImg.setAttribute('alt', '敗訴');
+      judge.style.width = size.haisoW + 'px';
+      judge.style.height = size.haisoH + 'px';
       lose = !lose;
     }
     if (ori.z >= 0 && lose) {
-      document.getElementById('judgement-img').setAttribute('src', 'shoso.png');
-      document.getElementById('judgement-img').setAttribute('alt', '勝訴');
+      judgeImg.setAttribute('src', 'shoso.png');
+      judgeImg.setAttribute('alt', '勝訴');
+      judge.style.width = size.shosoW + 'px';
+      judge.style.height = size.shosoH + 'px';
       lose = !lose;
     }
 
@@ -21,7 +34,7 @@ var orient = (function(ori) {
     if (lose)
       deg = 180 - deg;
 
-    document.getElementById('judgement').style.MozTransform = 'rotate(' + deg + 'deg)';
+    judge.style.MozTransform = 'rotate(' + deg + 'deg)';
   };
 })();
 
