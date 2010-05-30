886b13b19a842c91dee632cf4967354b3c811eca
Kosei Moriyama <cou929@gmail.com>
2010-05-30 20:25:39 +0900

    show "haiso" when turn macbook upside down

--- iShoso-moz/ishoso.js
+++ iShoso-moz/ishoso.js
@@ -1,10 +1,29 @@
-var orient = function(ori) {
-  document.getElementById('ori-x').innerHTML = ori.x;
-  document.getElementById('ori-y').innerHTML = ori.y;
-  document.getElementById('ori-z').innerHTML = ori.z;
+var orient = (function(ori) {
+  var lose = false;
 
-  document.getElementById('shoso').style.MozTransform = 'rotate(' + -1 * (ori.x * 90) + 'deg)';
-};
+  return function(ori) {
+    document.getElementById('ori-x').innerHTML = ori.x;
+    document.getElementById('ori-y').innerHTML = ori.y;
+    document.getElementById('ori-z').innerHTML = ori.z;
+
+    if (ori.z < 0 && !lose) {
+      document.getElementById('judgement-img').setAttribute('src', 'haiso.png');
+      document.getElementById('judgement-img').setAttribute('alt', '敗訴');
+      lose = !lose;
+    }
+    if (ori.z >= 0 && lose) {
+      document.getElementById('judgement-img').setAttribute('src', 'shoso.png');
+      document.getElementById('judgement-img').setAttribute('alt', '勝訴');
+      lose = !lose;
+    }
+
+    var deg = -1 * (ori.x * 90);
+    if (lose)
+      deg = 180 - deg;
+
+    document.getElementById('judgement').style.MozTransform = 'rotate(' + deg + 'deg)';
+  };
+})();
 
 window.addEventListener("load", function(){
                           window.addEventListener("MozOrientation", orient, false);
